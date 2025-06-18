629c3de28cc0ac9f4ca7066d7458f4dea81ca98f
Stefan Popescu <stefanpopescu99@gmail.com>
2025-06-18 16:45:51 +0300

    BE: lower max discover page

--- backend/src/main/java/com/andreea/twoplayermoviepicker/utils/ConfigVariables.java
+++ backend/src/main/java/com/andreea/twoplayermoviepicker/utils/ConfigVariables.java
@@ -3,6 +3,6 @@ package com.andreea.twoplayermoviepicker.utils;
 import com.uwetrottmann.tmdb2.enumerations.SortBy;
 
 public class ConfigVariables {
-    public static final Integer MAX_DISCOVER_PAGE = 500;
+    public static final Integer MAX_DISCOVER_PAGE = 300;
     public static final SortBy DISCOVER_SORT_BY = SortBy.POPULARITY_DESC;
 }
